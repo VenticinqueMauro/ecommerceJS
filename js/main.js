a9c7faadca65c5209b38e5000987325e066cb0a4
Venticinque Mauro <mauro.venticinque@hotmail.com>
2022-08-19 11:13:08 -0300

    modificando seccion contacto

--- js/main.js
+++ js/main.js
@@ -4,39 +4,39 @@ const productos = [
         marca: "vizzano",
         categoria: "Botas",
         precio: 11790,
-        color:"marron",
-        numero:34,
-        cantidad:1,
-        img: "img/botasvizMarron.jpg" 
+        color: "marron",
+        numero: 34,
+        cantidad: 1,
+        img: "img/botasvizMarron.jpg"
     },
     {
         id: 2,
         marca: "ramarin",
         categoria: "Sandalias",
         precio: 8590,
-        color:"jean",
-        numero:36,
-        cantidad:1,
-        img: "img/sandaliasRamarinJean.jpg" 
+        color: "jean",
+        numero: 36,
+        cantidad: 1,
+        img: "img/sandaliasRamarinJean.jpg"
     },
     {
         id: 3,
         marca: "ene",
         categoria: "Botas",
         precio: 12390,
-        color:"gris",
-        numero:38,
-        cantidad:1,
-        img: "img/enereptilgris.jpg" 
+        color: "gris",
+        numero: 38,
+        cantidad: 1,
+        img: "img/enereptilgris.jpg"
     },
     {
         id: 4,
         marca: "vizzano",
         categoria: "Sandalias",
         precio: 7480,
-        color:"roja",
-        numero:36,
-        cantidad:1,
+        color: "roja",
+        numero: 36,
+        cantidad: 1,
         img: "img/sandaliasVizRoja.jpg"
     },
 
@@ -45,19 +45,19 @@ const productos = [
         marca: "varietta",
         categoria: "Stilettos",
         precio: 6290,
-        color:"turquesa",
-        numero:39,
-        cantidad:1,
-        img: "img/stilettosVarTur.jpg" 
+        color: "turquesa",
+        numero: 39,
+        cantidad: 1,
+        img: "img/stilettosVarTur.jpg"
     },
     {
         id: 6,
         marca: "viauno",
         categoria: "Sandalias",
         precio: 9990,
-        color:"amarillo",
-        numero:36,
-        cantidad:1,
+        color: "amarillo",
+        numero: 36,
+        cantidad: 1,
         img: "img/viaUnoTachoChinoAmarillo.jpg"
     },
     {
@@ -65,9 +65,9 @@ const productos = [
         marca: "varietta",
         categoria: "Sandalias",
         precio: 8490,
-        color:"animal print",
-        numero:37,
-        cantidad:1,
+        color: "animal print",
+        numero: 37,
+        cantidad: 1,
         img: "img/sandaliasVarRojacnpelo.jpg"
     },
     {
@@ -75,9 +75,9 @@ const productos = [
         marca: "beira rio",
         categoria: "Sandalias",
         precio: 7490,
-        color:"negro",
-        numero:40,
-        cantidad:1,
+        color: "negro",
+        numero: 40,
+        cantidad: 1,
         img: "img/beiraTachoChino.jpg"
     },
     {
@@ -85,9 +85,9 @@ const productos = [
         marca: "varietta",
         categoria: "Stilettos",
         precio: 6490,
-        color:"amarillo",
-        numero:37,
-        cantidad:1,
+        color: "amarillo",
+        numero: 37,
+        cantidad: 1,
         img: "img/variettaamar.jpg"
     },
     {
@@ -95,55 +95,55 @@ const productos = [
         marca: "varietta",
         categoria: "Stilettos",
         precio: 5490,
-        color:"animal print",
-        numero:37,
-        cantidad:1,
-        img: "img/variettaanimalb.jpg" 
+        color: "animal print",
+        numero: 37,
+        cantidad: 1,
+        img: "img/variettaanimalb.jpg"
     },
     {
         id: 11,
         marca: "varietta",
         categoria: "Stilettos",
         precio: 6290,
-        color:"multicolor",
-        numero:37,
-        cantidad:1,
-        img: "img/variettamultic.jpg" 
+        color: "multicolor",
+        numero: 37,
+        cantidad: 1,
+        img: "img/variettamultic.jpg"
     },
     {
         id: 12,
         marca: "vizzano",
         categoria: "Sandalias",
         precio: 7480,
-        color:"blanco",
-        numero:39,
-        cantidad:1,
-        img: "img/viztacop.jpg" 
+        color: "blanco",
+        numero: 39,
+        cantidad: 1,
+        img: "img/viztacop.jpg"
     },
 ]
 
 class Productos {
-    constructor(id, marca, categoria, precio, color, numero, cantidad, img){
+    constructor(id, marca, categoria, precio, color, numero, cantidad, img) {
         this.id = id,
-        this.marca = marca,
-        this.categoria = categoria, 
-        this.precio = precio, 
-        this.color = color,
-        this.numero = numero,
-        this.cantidad = cantidad,
-        this.img = img
+            this.marca = marca,
+            this.categoria = categoria,
+            this.precio = precio,
+            this.color = color,
+            this.numero = numero,
+            this.cantidad = cantidad,
+            this.img = img
     }
 }
 
 // FUNCION PARA AGREGAR NUEVOS PRODUCTOS AL ARRAY DE PRODUCTOS MEDIANTE LA INSTANCIACION DE UN OBJETO
 
-function agregarAlStock(nombreProducto){
+function agregarAlStock(nombreProducto) {
     productos.push(nombreProducto)
 }
 
 // FUNCION PARA SELECCIONAR POR ID ELEMENTOS DEL DOM
 
-function selectID(id){
+function selectID(id) {
     return document.getElementById(id);
 }
 
@@ -164,13 +164,13 @@ const productos_categorias = [];
 // RECORRO Y RENDERIZO ARRAY DE PRODUCTOS
 
 productos.forEach((x) => {
-    
-    if (!productos_categorias.includes(x.categoria)){
+
+    if (!productos_categorias.includes(x.categoria)) {
         productos_categorias.push(x.categoria)
     }
 
     const div = document.createElement('div')
-    div.classList.add('col','col-md-3')
+    div.classList.add('col', 'col-md-3')
     div.innerHTML = `
             <div class="card mt-4 p-1 border-0 mx-auto" style="width: 15rem;">
                 <img src="${x.img}" class="card-img-top mx-auto d-block w-55" alt="${x.categoria}" >
@@ -188,56 +188,56 @@ productos.forEach((x) => {
     const btnCarrito = selectID(`btnAgregar${x.id}`)
     btnCarrito.addEventListener('click', () => {
         agregarAlCarrito(x.id)
-    } )
-} )
+    })
+})
 
 
 ////////////////////////////////////////////////////////////////////
 // ORDENO POR CATEGORIA EN EL BOTON PRODUCTOS DEL NAVBAR // FALTA COMPLETAR
 
 
-for (let i = 0 ; i < productos_categorias.length ; i++){
+for (let i = 0; i < productos_categorias.length; i++) {
     let contenido = ""
     contenido += `<li><a href="#" id="categoria${productos_categorias[i]}" class="text-decoration-none text-dark p-2">${productos_categorias[i]}<a></li>`
     categorias.innerHTML += (contenido)
 }
-document.getElementById(`categoriaBotas`).addEventListener ('click', yella)
-document.getElementById(`categoriaStilettos`).addEventListener ('click', yella)
-document.getElementById(`categoriaSandalias`).addEventListener ('click', yella)
+document.getElementById(`categoriaBotas`).addEventListener('click', yella)
+document.getElementById(`categoriaStilettos`).addEventListener('click', yella)
+document.getElementById(`categoriaSandalias`).addEventListener('click', yella)
 
-function yella(){
+function yella() {
     console.log("HOLA!!")
 }
 ////////////////////////////////////////////////////////////////////////
 
 // AGREGO PRODUCTOS AL CARRITO, EN CASO DE REPETIR ITEM LOS SUMO AL IGUAL QUE SU PRECIO.
 
-function agregarAlCarrito(id){
+function agregarAlCarrito(id) {
     Toastify({
-    gravity: "bottom",
-    position: "right",
-    text: "Producto Agregado",
-    duration: 2000,
-    style:{ borderRadius: "30px" }
+        gravity: "bottom",
+        position: "right",
+        text: "Producto Agregado",
+        duration: 2000,
+        style: { borderRadius: "30px" }
     }).showToast();
     const repetido = carrito.some((x) => x.id === id)
-    if (repetido){
-        const x = carrito.map (x => {
-            if (x.id === id){
+    if (repetido) {
+        const x = carrito.map(x => {
+            if (x.id === id) {
                 x.cantidad++
-                x.precio+= x.precio
+                x.precio += x.precio
             }
         })
     } else {
-    const item = productos.find((x) => x.id === id);
-    carrito.push(item);
+        const item = productos.find((x) => x.id === id);
+        carrito.push(item);
     }
     actualizarCarrito()
 }
 
 // ACTUALIZO Y RENDERIZO CARRITO EN MODAL. ALMACENO DATOS EN LA LOCALSTORAGE
 
-function actualizarCarrito(){
+function actualizarCarrito() {
     contenedorModal.innerHTML = "";
     carrito.forEach((x) => {
         let html = document.createElement('div')
@@ -266,16 +266,16 @@ function actualizarCarrito(){
 
 // ELIMINAR INDIVIDUALMENTE CADA PRODUCTO DEL CARRITO 
 
-function eliminarProducto(id){
+function eliminarProducto(id) {
     Toastify({
         text: "Producto Eliminado",
         duration: 1000,
-        style: { 
+        style: {
             // background: "#ff000094",
             background: "linear-gradient(to right, #ff000079, red)",
             borderRadius: "30px"
         },
-        }).showToast();
+    }).showToast();
     const item = carrito.find(x => x.id === id);
     const indice = carrito.indexOf(item)
     carrito.splice(indice, 1);
@@ -301,7 +301,7 @@ vaciarCarrito.addEventListener('click', () => {
 // OBTIENE LOS DATOS DE LOCALSTORAGE
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('carrito')){
+    if (localStorage.getItem('carrito')) {
         carrito = JSON.parse(localStorage.getItem('carrito'))
         actualizarCarrito()
     }
@@ -329,44 +329,49 @@ arrayCategorias.sort((a, b) => {
 
 const contacto = selectID('contacto')
 
-contacto.addEventListener ('click', renderContacto)
+contacto.addEventListener('click', renderContacto)
 
-function renderContacto(){
-    let contact = document.createElement('div')
-    contact.classList.add('container')
-    contact.innerHTML = `
-    <div class="modal fade" id="contact" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content ">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">CONTACTO</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="mb-3">
-                            <label for="recipient-name" class="col-form-label">Alias:</label>
-                            <input type="text" class="form-control" id="recipient-name">
-                        </div>
-                        <div class="mb-3">
-                            <label for="recipient-email" class="col-form-label">E-mail:</label>
-                            <input type="email" class="form-control" id="recipient-name">
-                        </div>
-                        <div class="mb-3">
-                            <label for="message-text" class="col-form-label">Consulta:</label>
-                            <textarea class="form-control" id="message-text"></textarea>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar <i class="fa-solid fa-xmark"></i></button>
-                    <button type="button" class="btn btn-primary">Enviar <i class="fa-solid fa-paper-plane"></i></button>
-                </div>
-            </div>
+function renderContacto() {
+    Swal.fire({
+        imageUrl: 'img/mail.gif',
+        imageAlt: 'imagen de contacto',
+        imageWidth: 150,
+        html: `
+        
+
+        <div class="mb-3 text-start containerName">
+            <label  class="form-label"></label>
+            <input type="name" class="inputName"  placeholder="Alias">
+            <span class="bottom"></span>
+            <span class="right"></span>
+            <span class="top"></span>
+            <span class="left"></span>
+            
         </div>
-    </div>
-    `
-    contacto.append(contact)
+        <div class="containerName text-start">
+            <label  class="form-label"></label>
+            <input type="email" class="inputName"  placeholder="name@example.com">
+            <span class="bottom"></span>
+            <span class="right"></span>
+            <span class="top"></span>
+            <span class="left"></span>      
+        </div>
+        <div class="mb-3 text-start containerName" style="margin-top: 15px;">
+            <label class="form-label"></label>
+            <textarea class="inputName"  rows="3" placeholder="Mensaje"></textarea>
+            <span class="bottom"></span>
+            <span class="right"></span>
+            <span class="top"></span>
+            <span class="left"></span>
+        </div>
+        `,
+        showCancelButton: false,
+        focusConfirm: false,
+        confirmButtonText:
+            '<i class="fa-solid fa-paper-plane"></i> Enviar!',
+        confirmButtonAriaLabel: 'Enviar Consulta',
+        confirmButtonColor: '#0d6efd',
+    })
 }
 
 
@@ -376,18 +381,17 @@ const nosotros = selectID('nosotros')
 
 nosotros.addEventListener('click', renderNosotros)
 
-function renderNosotros(){
+function renderNosotros() {
     Swal.fire({
         title: 'Te Preguntas quienes somos?',
         imageUrl: 'img/family.gif',
-        imageWidth: 250 ,
-        imageHeigth: 100 ,
+        imageWidth: 250,
         text: 'Somos un matrimonio tucumano que se dedica al rubro femenino desde hace 4 años. Sabemos QUE y COMO hacer felices a nuestras clientas! y claro eso viene acompañado siempre de Calidad, Glamour y un excelente precio de competencia. Te invitamos a que conozcas nuestros productos si aun no lo has hecho!!',
         showClass: {
-        popup: 'animate__animated animate__zoomInDown'
+            popup: 'animate__animated animate__zoomInDown'
         },
         hideClass: {
-        popup: 'animate__animated animate__zoomOut'
+            popup: 'animate__animated animate__zoomOut'
         },
         confirmButtonColor: '#0d6efd',
         confirmButtonText: '<i class="fa fa-thumbs-up"></i> Genial!',
